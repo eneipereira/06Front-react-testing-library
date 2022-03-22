cb1a8c11239e5252fc8898d804344eb51edb9f5c
Enei <eneipereira@outlook.com.br>
2022-03-22 19:47:14 -0300

    Realiza requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+const MAX_ID = 25;
+const ARR_LENGTH = 4;
+
+const fav = pokemons.filter(({ id }) => id <= MAX_ID);
+
+describe('Testa o componente `FavoritePokemons`', () => {
+  it('deve exibir na tela a mensagem `No favorite pokemon found`,'
+  + 'se não houverem pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeDefined();
+  });
+  it('deve exibir na tela os cards dos pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ fav } />);
+
+    const favPokes = screen.getAllByTestId('pokemon-name');
+    expect(favPokes).toHaveLength(ARR_LENGTH);
+  });
+});
